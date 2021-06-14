5d11c3b16c428b367d91dbd5a60be41502e7e279
masashiabe <tech.abe.jp@gmail.com>
2021-06-15 02:28:04 +0900

    8kyu Quarter of the year

---
+++ 8kyu/answer/QuarterOfTheYear.js
@@ -0,0 +1,16 @@
+// Best Practice
+const quarterOf = m => Math.ceil(m/3);
+
+// Second Best Practice
+const quarterOf = (month) => {
+    if (month <= 3) {
+      return 1
+    } else if (month <= 6) {
+      return 2
+    } else if (month <= 9) {
+      return 3
+    } else if (month <= 12) {
+      return 4
+    }
+    
+  }
